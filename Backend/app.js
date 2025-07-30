7a4aad587247ec652bb1a70196931119282a94c1
Vishal Jha <jhav8422@gmail.com>
2025-07-30 13:42:43 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended : true}))
 app.use(cookieparser())
 app.use(express.static('public'))
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://brandpost.netlify.app',
   credentials: true 
 }));
 
