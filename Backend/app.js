23d798de4f600e0da80a4b40ef958e1e75a52b65
Vishal Jha <jhav8422@gmail.com>
2025-08-01 09:45:18 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -17,14 +17,21 @@ app.use(express.urlencoded({extended : true}))
 app.use(cookieparser())
 app.use(express.static('public'))
 
-const allowedOrigin = '*'
+const allowedOrigins = ['http://localhost:3000', 'https://brandpost.netlify.app']
 
 app.use(cors({
-  origin: allowedOrigin,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
+
 
 // ✅ Handle Preflight Requests
 // app.options('*', cors({
